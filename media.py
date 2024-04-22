4bc0f6ca7a999074d6c7e2ab02a9b41b7d58be0b
Marcelo Nascimento <marcelo.ngomes@hotmail.com>
2024-04-21 21:16:28 -0300

    iniciando um sistema que calcula a média de um aluno

---
+++ media.py
@@ -0,0 +1,10 @@
+print('Secretaria')
+print()
+aluno = input('Digite o nome do aluno: ')
+print('Bem vindo', aluno)
+n1 = float(input('Digite a nota 1: '))
+n2 = float(input('Digite a nota 2: '))
+media = (n1 + n2)/2
+print('A média do aluno {} é {}'.format(aluno,media))
+
+
