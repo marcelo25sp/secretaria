8441bba3bbfb295f741d322e649e8f24425ad9c2
Marcelo Nascimento <marcelo.ngomes@hotmail.com>
2024-04-22 21:50:35 -0300

    Adicionando melhorias ao código

--- media.py
+++ media.py
@@ -1,10 +1,11 @@
-print('Secretaria')
-print()
+print('Secretaria Escola Futuro')
+print('*'*100)
 aluno = input('Digite o nome do aluno: ')
 print('Bem vindo', aluno)
 n1 = float(input('Digite a nota 1: '))
 n2 = float(input('Digite a nota 2: '))
 media = (n1 + n2)/2
 print('A média do aluno {} é {}'.format(aluno,media))
+
 
 
