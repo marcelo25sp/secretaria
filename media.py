b13fa7d32a4e965cc0430b2b34daad540b0d1372
Marcelo Nascimento <marcelo.ngomes@hotmail.com>
2024-04-23 22:16:28 -0300

    Adicionando comandos de decisão ao código

--- media.py
+++ media.py
@@ -7,5 +7,12 @@ n2 = float(input('Digite a nota 2: '))
 media = (n1 + n2)/2
 print('A média do aluno {} é {}'.format(aluno,media))
 
+if media >= 7:
+    print('Aluno aprovado!')
+else:
+    print('Aluno reprovado')
+
+
+
 
 
